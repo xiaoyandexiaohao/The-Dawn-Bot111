c6271c13785cfda25c4e48b685dbc965a7791e9c
yanpengcloud <2111303223@qq.com>
2024-12-27 13:53:12 +0800

    Update logger.py

--- console/logger.py
+++ console/logger.py
@@ -2,11 +2,11 @@ from colorama import Fore
 
 
 def error_log(message: str):
-    print(Fore.RED + ">> ERROR |" + Fore.LIGHTBLACK_EX + f" {message}")
+    print(Fore.RED + ">> 错误 |" + Fore.LIGHTBLACK_EX + f" {message}")
 
 
 def success_log(message: str):
-    print(Fore.GREEN + ">> SUCCESS |" + Fore.LIGHTBLACK_EX + f" {message}")
+    print(Fore.GREEN + ">> 成功 |" + Fore.LIGHTBLACK_EX + f" {message}")
 
 
 def info_log(message: str):
